b6eca8e63876293557568b578ca9e1afd44f8a11
Per Gröningsson <per.groningsson@gmail.com>
2015-09-01 23:54:21 +0200

    dispfiles.c executes exiftool

--- dispfiles.c
+++ dispfiles.c
@@ -6,6 +6,10 @@
 #include <stdio.h> 
 #include <string.h>
 
+char *trimwhitespace(char *);
+
+
+
 int main(void)
 {
   DIR           *d;
@@ -16,10 +20,11 @@ int main(void)
     while ((dir = readdir(d)) != NULL)
     {
       //printf("%s\n", dir->d_name);
+      char *fname = dir->d_name;
 
       char cmd[128];
       strcpy(cmd,"file -b \"");
-      strcat(cmd,dir->d_name);
+      strcat(cmd,fname);
       strcat(cmd,"\"");
       //printf("%s\n",cmd);
       FILE *in = popen(cmd,"r");
@@ -29,10 +34,44 @@ int main(void)
       }
       pclose(in);
 
-      if (strstr(buff,"image data") != NULL)
+      if (strstr(buff,"image data") != NULL) // only list image files
 	{
-	  printf("%s\n", dir->d_name);
-	  printf("%s", buff);
+	  //printf("%s\n", fname);
+	  //printf("%s", buff);
+
+	  char deliml[] = "(";
+	  char delimr[] = ")";
+
+	  if (strstr(fname,deliml) != NULL && strstr(fname,delimr) != NULL) //file has correct format
+	    {
+	      
+	      char *p = strstr(fname,".");
+	      char *fext = ++p;
+	      //printf("%s\n",fext);
+
+	      char imfile[128];
+	      strcpy(imfile,fname);
+	      //puts(imfile);
+	      char *imtitle = strtok(fname,deliml);
+	      trimwhitespace(imtitle);
+	      //puts(imfile);
+	    
+	      char *datime = strtok(NULL,delimr);
+	      trimwhitespace(datime);
+	      //puts(datime);
+
+	      char exif_cmd[128];
+	      strcpy(exif_cmd,"exiftool ");
+	      strcat(exif_cmd,"\"-DateTimeOriginal=");
+	      strcat(exif_cmd,datime);
+	      strcat(exif_cmd,"\" \"");
+	      strcat(exif_cmd,imfile);
+	      strcat(exif_cmd,"\"");
+	      puts(exif_cmd);
+	      system(exif_cmd);
+	      system("\\rm -f *_original*");
+	    }
+
 	}
       
     }
@@ -42,3 +81,26 @@ int main(void)
 
   return(0);
 }
+
+
+
+char *trimwhitespace(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace(*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace(*end)) end--;
+
+  // Write new null terminator
+  *(end+1) = 0;
+
+  return str;
+}
+
